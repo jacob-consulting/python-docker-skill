3b00981a01bc784fd67b4f68e396096a97a9ed23
Alexander Jacob <alexander.jacob@jacob-consulting.de>
2026-04-22 08:58:26 +0200

    test: add Author model and list view tests

---
+++ src/demo/tests/test_author.py
@@ -0,0 +1,21 @@
+import pytest
+from django.contrib.auth.models import Permission, User
+from django.urls import reverse
+
+from demo.models import Author
+
+
+@pytest.mark.django_db
+def test_author_str():
+    author = Author(first_name="Jane", last_name="Doe")
+    assert str(author) == "Jane Doe"
+
+
+@pytest.mark.django_db
+def test_author_list_view(client):
+    user = User.objects.create_user(username="tester", password="secret")
+    perm = Permission.objects.get(codename="view_author")
+    user.user_permissions.add(perm)
+    client.force_login(user)
+    response = client.get(reverse("author-list"))
+    assert response.status_code == 200
